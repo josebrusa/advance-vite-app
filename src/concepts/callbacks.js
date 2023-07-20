4223e363ef4ea4bcefe7999ea3d17ea73e54f4f5
josebrusa <josebrusa@outlook.com>
2023-07-20 12:57:47 +0200

    Callbacks

--- src/concepts/callbacks.js
+++ src/concepts/callbacks.js
@@ -7,18 +7,26 @@ import { heroes } from "../data/heroes";
 
 export const callbacksComponent = (element) => {
     const id = "5d86371f9f80b591f499df32";
-    findHero(id, ({ name }) => {
-        element.innerHTML = name;
+    findHero(id, (error, hero) => {
+        if (error) {
+            element.innerHTML = error;
+            return;
+        }
+        element.innerHTML = hero.name;
     });
 };
 
 /**
  *
  * @param {String} id
- * @param { (hero: Object) => void} callback
+ * @param { (error: String|null, hero: Object) => void} callback
  */
 const findHero = (id, callback) => {
     const hero = heroes.find((hero) => hero.id === id);
+    if (!hero) {
+        callback(`Hero with id ${id} not found`);
+        return;
+    }
 
-    callback(hero);
+    callback(null, hero);
 };
